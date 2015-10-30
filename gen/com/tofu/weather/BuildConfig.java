d6f83047cca44802220995e17fa1312d8834d6a4
MockingTofu <qq87502872@gmail.com>
2015-10-30 22:31:26 +0800

    First commit.

---
+++ gen/com/tofu/weather/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tofu.weather;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/db/WeatherOpenHelper.java
@@ -0,0 +1,39 @@
+package db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class WeatherOpenHelper extends SQLiteOpenHelper {
+	
+	public WeatherOpenHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	//Province
+	public static final String CREATE_PROVINCE="create table Province ("+"id integer primary key autoincrement, "+"province_name text, "+"province_code text)";
+	
+	//City
+	public static final String CREATE_CITY="create table City ("+"id integer primary key autoincrement, "+"city_name text, "+"city_code text, "+"province_id integer)";
+	
+	//Country
+	public static final String CREATE_COUNTRY="create table Country ("+"id integer primary key autoincrement, "+"country_name text, "+"country_code text, "+"city_id integer)";
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(CREATE_PROVINCE);	//Create Table Province
+		db.execSQL(CREATE_CITY);		//Create Table City
+		db.execSQL(CREATE_COUNTRY);	//Create Table Country
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
