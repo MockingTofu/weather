72618f24b0b173bbf23ff09c2ea3d69f110bdb88
MockingTofu <qq87502872@gmail.com>
2015-11-08 22:55:45 +0800

    增加后台自动更新功能
    Signed-off-by: MockingTofu <qq87502872@gmail.com>

--- src/activity/WeatherActivity.java
+++ src/activity/WeatherActivity.java
@@ -14,6 +14,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import service.AutoUpdateService;
 import util.HttpCallbackListener;
 import util.HttpUtil;
 import util.Utility;
@@ -176,5 +177,11 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		
+		/**
+		 * ¼¤»îAutoUpdateService·þÎñ
+		 */
+		Intent intent=new Intent(this,AutoUpdateService.class);
+		startService(intent);
 	}
 }
